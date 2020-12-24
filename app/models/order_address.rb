0927b21417811f2314e1a717befc228e0433ca24
HIDEKAZU-YARITA <hiyarita@nifty.com>
2020-12-24 09:17:39 +0900

    エラー表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,11 @@ class OrderAddress
 
   with_options presence: true do
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 0, message: 'must be selected.' }
     validates :city
     validates :house_number
     validates :phone_number,  format: {with: /\A\d{10}$|^\d{11}\z/, message: "is invalid."}
   end
-
-  validates :prefecture_id, numericality: { other_than: 0, message: 'must be selected.' }
 
   def save
     @order = Order.create(item_id: item_id, user_id: user_id)

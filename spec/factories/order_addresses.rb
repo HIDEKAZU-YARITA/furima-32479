4c455ee44a3067a4adf806d52592d86c87c535ef
HIDEKAZU-YARITA <hiyarita@nifty.com>
2020-12-24 18:26:55 +0900

    テストコードの実装：決済処理分追加

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     house_number          { "千代田1-1-1" }
     building_name         { '東京ビル' }
     phone_number          { '0333333333' }
+    token                 {"tok_abcdefghijk00000000000000000"}
   end
 end

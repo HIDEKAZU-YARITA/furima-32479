6186b9f640b4be12a100bb8e2273989361867d5d
HIDEKAZU-YARITA <hiyarita@nifty.com>
2020-12-25 12:26:28 +0900

    テストコードの修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,5 +7,7 @@ FactoryBot.define do
     building_name         { '東京ビル' }
     phone_number          { '0333333333' }
     token                 { 'tok_abcdefghijk00000000000000000' }
+    user_id               { 1 }
+    item_id               { 1 }
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -102,10 +102,23 @@ RSpec.describe OrderAddress, type: :model do
           @order_address.valid?
           expect(@order_address.errors.full_messages).to include('Phone number is invalid.')
         end
-        it 'トークンないと保存できないこと' do
+
+        it 'トークンがないと保存できないこと' do
           @order_address.token = nil
           @order_address.valid?
           expect(@order_address.errors.full_messages).to include("Token can't be blank")
+        end
+
+        it 'user_idがないと保存できないこと' do
+          @order_address.user_id = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("User can't be blank")
+        end
+
+        it 'item_idがないと保存できないこと' do
+          @order_address.item_id = nil
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Item can't be blank")
         end
       end
     end

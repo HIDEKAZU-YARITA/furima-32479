3aae2c2da4a29d49785048b823f0f634b3bc7c32
HIDEKAZU-YARITA <hiyarita@nifty.com>
2021-01-06 16:33:50 +0900

    (追加実装：ユーザー情報編集機能)デプロイ後不具合対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, only: :show
+  
   def show
+  end
+
+  private
+
+  def move_to_index
     @user = User.find(params[:id])
+    redirect_to root_path unless @user.id == current_user.id
   end
 end
